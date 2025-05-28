08466c2e6951bb33e5bedfb80eba50d124c3f9fd
Sam Hart <sam@samhart.co.uk>
2025-05-28 10:04:49 +0100

    Add basic line drawing code
    I added basic code to draw 3 white lines that form a triangle.

--- paths.py
+++ paths.py
@@ -6,4 +6,10 @@ window = pygame.display.set_mode((500, 400))
 
 while True:
     
+    pygame.draw.line(window, (255, 255, 255), (50, 50), (75, 75), True)
+    
+    pygame.draw.line(window, (255, 255, 255), (75, 75), (25, 75), True)
+    
+    pygame.draw.line(window, (255, 255, 255), (25, 75), (50, 50), True)
+    
     pygame.display.update()
